13f51fafff3564b60099eba64f703e6ffc25d6c6
01VitorSam <93853950+01VitorSam@users.noreply.github.com>
2023-08-09 02:21:25 -0300

    Update
    Nova atualização em um ex

--- JavaScript/ex012/script/script.js
+++ JavaScript/ex012/script/script.js
@@ -1,18 +1,27 @@
 function enviar() {
+    var agora = new Date()
+    var agora2 = new Date()
+    var agora2 = agora2.getMinutes()
+    var hora = agora.getHours()
     var val = document.getElementById('valor')
     var res = document.getElementById('resposta')
-    var valo = parseFloat(val.value)
-    if (valo >= 12 && valo <= 17) {
-        res.innerHTML = `Boa Tarde 🌞`
+    var hor = document.getElementById('hora')
+    var valo = String(val.value)
+    if (valo >= 13 && valo <= 17) {
+        res.innerHTML = `Agora são ${hora}h e ${agora2}m Boa Tarde ⛅`
+        hor.innerHTML = valo
     } else {
-        if (valo > 17 && valo <= 23) {
-            res.innerHTML = `boa Noite 🌃`
+        if (hora > 17 && hora <= 23) {
+            res.innerHTML = `Agora são ${hora}h e ${agora2}m Boa Noite 🌃`
+            hor.innerHTML = valo
         } else {
-            if (valo >= 0  && valo <= 5 ) {
-                res.innerHTML = `Boa Madrugada ⛅`
+            if (hora >= 0  && hora <= 5 ) {
+                res.innerHTML = `Agora são ${hora}h e ${agora2}m Boa Madrugada ⛅`
+                hor.innerHTML = valo
             } else {
-                if (valo > 5 && valo < 13) {
-                    res.innerHTML = `Bom dia 🌞`
+                if (hora > 5 && hora < 13) {
+                    res.innerHTML = `Agora são ${hora}h e ${agora2}m Bom Dia 🌞`
+                    hor.innerHTML = valo
                 } else {
                     res.innerHTML = `Essa Hora não existe ou não esta dentro do padrão do Sistema!`
                 }
