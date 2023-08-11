04a3ca2d622602d83ad66aafa3cde1f27600f31c
01VitorSam <93853950+01VitorSam@users.noreply.github.com>
2023-08-11 01:12:05 -0300

    Update
    novos comandos aprendidos

--- JavaScript/ex013/script/script.js
+++ JavaScript/ex013/script/script.js
@@ -1,14 +1,38 @@
-function dia() {
-    var agora = new Date()
-    var semana = agora.getDay()
-    var dia = document.getElementById('diaSem')
-    var DIA = String(dia.value)
-    switch (semana){
-        case 4:
-            console.log('ola')
+function acao(){
+    var data = new Date()
+    var agora = data.getDay()
+    var day = document.getElementById('dia')
+    var comp = document.getElementById('completo')
+    switch (agora) {
+        case 0 :
+            day.innerHTML = 'Domingo'
+            comp.innerHTML = data
             break
-        default:
-            console.log('Deu ruim!')
+        case 1 :
+            day.innerHTML = 'Segunda'
+            comp.innerHTML = data
             break
+        case 2 : 
+            day.innerHTML = 'Terça'
+            comp.innerHTML = data
+            break
+        case 3 :
+            day.innerHTML = 'Quarta'
+            comp.innerHTML = data
+            break
+        case 4 :
+            day.innerHTML = 'Quinta'
+            comp.innerHTML = data
+            break
+        case 5 :
+            day.innerHTML = 'Sexta'
+            comp.innerHTML = data
+            break
+        case 6 :
+            day.innerHTML = 'Sabádo'
+            comp.innerHTML = data
+            break
+        default :
+            day.innerHTML = 'Erro dia Invalido!'
     }
 }
