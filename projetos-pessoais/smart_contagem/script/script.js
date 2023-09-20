c8a33897f3a9bc451dbfed45a148b18b2cf56ba4
Vitor Dias <temadark2022@gmail.com>
2023-09-20 02:27:49 -0300

    Update
    Novas atividades feitas e novos aprendizados

---
+++ projetos-pessoais/smart_contagem/script/script.js
@@ -0,0 +1,10 @@
+function calcular() {
+    var number = document.getElementById('NUMBER')
+    var end = document.getElementById('END')
+    var step = document.getElementById('STEP')
+
+    while(number <= end) {
+        console.log(number)
+        number++
+    }
+}
