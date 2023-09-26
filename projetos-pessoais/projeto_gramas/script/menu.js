30c7df4645ce202f185bb8b3bf69d223031115f7
Vitor Sam <temadark2022@gmail.com>
2023-09-25 22:59:43 -0300

    update
    update

--- projetos-pessoais/projeto_gramas/script/menu.js
+++ projetos-pessoais/projeto_gramas/script/menu.js
@@ -1,6 +1,18 @@
 function menuAbre() {
     let list = document.getElementById('lista')
+    let nav = document.getElementById('nav')
 
-    list.style.display = 'inline-block'
+        list.style.display = 'inline-block'
+
+        nav.style.backgroundColor = 'beige'
+
+}
+
+function menuFecha() {
+    let list = document.getElementById('lista')
+
+        list.style.display = 'none'
+
+        nav.style.backgroundColor = 'transparent'
 
 }
