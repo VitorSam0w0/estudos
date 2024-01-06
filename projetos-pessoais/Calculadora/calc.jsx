91e8025d732deaaa6321eb8e7f13622cd10491ef
Vitor Dias <temadark2022@gmail.com>
2024-01-06 00:35:22 -0300

    Calculadora
    Fazendo uma calculadora

---
+++ projetos-pessoais/Calculadora/calc.jsx
@@ -0,0 +1,31 @@
+function showNav() {
+    var invisible = window.document.getElementById("inner-nav")
+    invisible.classList.remove("invisible")
+    invisible.classList.add("visible")
+}
+
+function hideNav() {
+    var invisible = window.document.getElementById("inner-nav")
+    invisible.classList.remove("visible")
+    invisible.classList.add("invisible")
+}
+
+function numbers() {
+    var zero = document.getElementById("zero")
+    var one = document.getElementById("um")
+    var two = document.getElementById("two")
+    var three = document.getElementById("trhee")
+    var four = document.getElementById("four")
+    var five = document.getElementById("five")
+    var six = document.getElementById("six")
+    var seven = document.getElementById("seven")
+    var eigth = document.getElementById("eight")
+    var nite = document.getElementById("nine")
+
+    var more = document.getElementById("more")
+    var ad = document.getElementById("ad")
+    var sb = document.getElementById("sb")
+    var dv = document.getElementById("dv")
+
+    var result = document.getElementById("result")
+}
